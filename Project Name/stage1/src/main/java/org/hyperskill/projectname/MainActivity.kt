e681a75fe29b35588ff2a8be3face88456652396
RazoTRON <vlad05966@gmail.com>
2022-11-18 22:05:33 +0200

    add Jetpack Compose compatibility - increased version compileSdkVersion to 33 - increased version to minSdkVersion 21 - add dependencies - updated MainActivity.kt with default Compose template with Themes support

--- Project Name/stage1/src/main/java/org/hyperskill/projectname/MainActivity.kt
+++ Project Name/stage1/src/main/java/org/hyperskill/projectname/MainActivity.kt
@@ -1,11 +1,40 @@
 package org.hyperskill.projectname
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.hyperskill.secretdiary.ui.theme.HyperskillTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            HyperskillTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Hyperskill")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text("Hello, $name")
+}
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    HyperskillTheme {
+        Greeting(name = "Hyperskill")
     }
 }
